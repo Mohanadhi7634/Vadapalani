6504f6d11574f9e144c499ed618e710fd37ccdf6
Mohanadhi7634 <mohanadhi76342@gmail.com>
2025-09-29 17:46:43 +0530

    Added debug logs for webhook and message handling

--- server.js
+++ server.js
@@ -10,5 +10,7 @@ app.use(bodyParser.json());
 // Routes
 app.use('/webhook', webhookRoutes);
 
+
+//post m
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
